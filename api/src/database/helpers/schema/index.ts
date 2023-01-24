75712b3ff952ef26eafe8bd57f0ca741f82294af
petr.hulinsky <petr.hulinsky@koala42.com>
2023-01-24 15:16:33 +0100

    FE Only

--- api/src/database/helpers/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export { SchemaHelperDefault as postgres } from './dialects/default';
-export { SchemaHelperCockroachDb as cockroachdb } from './dialects/cockroachdb';
-export { SchemaHelperDefault as redshift } from './dialects/default';
-export { SchemaHelperOracle as oracle } from './dialects/oracle';
-export { SchemaHelperSQLite as sqlite } from './dialects/sqlite';
-export { SchemaHelperMySQL as mysql } from './dialects/mysql';
-export { SchemaHelperMSSQL as mssql } from './dialects/mssql';
--- api/src/utils/get-graphql-query-and-variables.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from 'express';
-import { pick } from 'lodash';
-
-export function getGraphqlQueryAndVariables(req: Request) {
-	const isGet = req.method?.toLowerCase() === 'get';
-	return pick(isGet ? req.query : req.body, ['query', 'variables']);
-}
